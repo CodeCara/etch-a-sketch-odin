664b9e85a672150175dc689856136f36fe225484
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-07-02 09:54:08 +0200

    Basic sketch functionality created

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const gridContainer = document.createElement('div');
+gridContainer.classList.add("gridContainer"); 
 const body = document.querySelector('body');
 body.appendChild(gridContainer);
 
 for (i=0; i<256; i++){
     const div = document.createElement('div'); 
-    div.innerText = parseInt(`${[i+1]}`);
+    div.classList.add("square");
+   // div.innerText = parseInt(`${[i+1]}`);
+   //div.innerText = "X";
     gridContainer.appendChild(div);
+    div.addEventListener("mouseover", hoverEffect);
+function hoverEffect (){
+    div.setAttribute("style", "background-color:green;")}
     //div.style.backgroundColor= "blue";
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
